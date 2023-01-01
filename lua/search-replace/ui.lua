6d08d318604f8db2097afcada9f20af3478b71b1
Rob Wilson <roobert@gmail.com>
2023-01-01 12:12:23 +0000

    Update deprecated API calls

--- lua/search-replace/ui.lua
+++ lua/search-replace/ui.lua
@@ -4,7 +4,7 @@ local state = { window_open = false }
 
 local util = require("search-replace.util")
 
-local buf, win
+local buf_id, win_id
 
 local function set_keymap(mode, cword, cexpr, cfile, cWORD)
 	local search_replace_function = ""
@@ -25,14 +25,20 @@ local function set_keymap(mode, cword, cexpr, cfile, cWORD)
 	}
 
 	for k, v in pairs(mappings) do
-		vim.api.nvim_buf_set_keymap(buf, "n", k, ':lua require("search-replace.ui").close()<cr> | :lua ' .. v .. "<cr>", {
-			nowait = true,
-			noremap = true,
-			silent = true,
-		})
+		vim.api.nvim_buf_set_keymap(
+			buf_id,
+			"n",
+			k,
+			':lua require("search-replace.ui").close()<cr> | :lua ' .. v .. "<cr>",
+			{
+				nowait = true,
+				noremap = true,
+				silent = true,
+			}
+		)
 	end
 
-	vim.api.nvim_buf_set_keymap(buf, "n", "q", ":lua require('search-replace.ui').close()<cr>", {
+	vim.api.nvim_buf_set_keymap(buf_id, "n", "q", ":lua require('search-replace.ui').close()<cr>", {
 		nowait = true,
 		noremap = true,
 		silent = true,
@@ -51,26 +57,26 @@ local function create_win()
 	-- * auto-calculate window buffer size
 	vim.api.nvim_command("5split +enew")
 
-	win = vim.api.nvim_get_current_win()
-	buf = vim.api.nvim_get_current_buf()
+	win_id = vim.api.nvim_get_current_win()
+	buf_id = vim.api.nvim_get_current_buf()
 
-	vim.api.nvim_buf_set_name(buf, "SearchReplace selections")
+	vim.api.nvim_buf_set_name(buf_id, "SearchReplace selections")
 
-	vim.api.nvim_win_set_option(win, "wrap", false)
-	vim.api.nvim_win_set_option(win, "cursorline", true)
-	vim.api.nvim_win_set_option(win, "number", false)
-	vim.api.nvim_win_set_option(win, "relativenumber", false)
+	vim.api.nvim_set_option_value("wrap", false, { win = win_id })
+	vim.api.nvim_set_option_value("cursorline", false, { win = win_id })
+	vim.api.nvim_set_option_value("number", false, { win = win_id })
+	vim.api.nvim_set_option_value("relativenumber", false, { win = win_id })
 
-	vim.api.nvim_buf_set_option(buf, "buftype", "nofile")
-	vim.api.nvim_buf_set_option(buf, "swapfile", false)
-	vim.api.nvim_buf_set_option(buf, "bufhidden", "wipe")
-	vim.api.nvim_buf_set_option(buf, "filetype", "nvim-search-replace")
-	vim.api.nvim_buf_set_option(buf, "modifiable", false)
+	vim.api.nvim_set_option_value("buftype", "nofile", { buf = buf_id })
+	vim.api.nvim_set_option_value("swapfile", false, { buf = buf_id })
+	vim.api.nvim_set_option_value("bufhidden", "wipe", { buf = buf_id })
+	vim.api.nvim_set_option_value("filetype", "nvim-search-replace", { buf = buf_id })
+	vim.api.nvim_set_option_value("modifiable", false, { buf = buf_id })
 end
 
 M.close = function()
-	if win and vim.api.nvim_win_is_valid(win) then
-		vim.api.nvim_win_close(win, true)
+	if win_id and vim.api.nvim_win_is_valid(win_id) then
+		vim.api.nvim_win_close(win_id, true)
 	end
 
 	state["window_open"] = false
@@ -94,9 +100,9 @@ local function selections(mode)
 	table.insert(list, #list + 1, "[f]ile:   " .. cfile)
 	table.insert(list, #list + 1, "[W]ORD:   " .. cWORD)
 
-	vim.api.nvim_buf_set_option(buf, "modifiable", true)
-	vim.api.nvim_buf_set_lines(buf, 0, -1, false, list)
-	vim.api.nvim_buf_set_option(buf, "modifiable", false)
+	vim.api.nvim_buf_set_option(buf_id, "modifiable", true)
+	vim.api.nvim_buf_set_lines(buf_id, 0, -1, false, list)
+	vim.api.nvim_buf_set_option(buf_id, "modifiable", false)
 end
 
 M.single_buffer_selections = function()
